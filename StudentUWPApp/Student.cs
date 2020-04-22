bf634f944cf8b9f47c4cdd7789accfe254f787e1
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-04-22 11:23:20 +0200

    tilføjet property navn og   OnPropertyChanged()

--- StudentUWPApp/Student.cs
+++ StudentUWPApp/Student.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudentUWPApp
 {
-    class Student
+    public class Student : INotifyPropertyChanged
     {
+
+        private string name;
+
+        public string Name {
+
+            get => this.name;
+
+            set
+            {
+                this.name = value;
+                OnPropertyChanged();
+            }
+
+
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        => this.PropertyChanged?.Invoke(this,
+            new PropertyChangedEventArgs(propertyName));
+
+
     }
 }
